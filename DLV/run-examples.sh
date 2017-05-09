5d4dc878812b0963cb70cdba48396a3c08572c4f
Bertram Ludaescher <ludaesch@illionis.edu>
2017-05-09 08:21:38 -0500

    renaming TAP input files

--- DLV/run-examples.sh
+++ DLV/run-examples.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in examples/*/*.dlv
+for file in examples/*/*.input
 do
 #  dlv rcc5 bl sd de "$file" -silent -stats -v -filter=e,o,pp 2> "$file".stats > "$file".out  # Don't use if |PWs| is large ... 
   dlv rcc5 bl sd de "$file" -silent -stats -v -filter=e,o,pp 2> "$file".stats > /dev/null
