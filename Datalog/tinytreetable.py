046ac582103f9e78582157845a58f563c262df4c
Shawn Bowers <smbowers@gmail.com>
2017-05-24 12:18:29 -0700

    bottom up inference progress, working on top down

--- Datalog/tinytreetable.py
+++ Datalog/tinytreetable.py
@@ -17,93 +17,26 @@ pp = 'pp'                       # proper part
 pi = 'pi'                       # proper part inverse
 
 
-table = {dr: {dr: {dr,po,eq,pi,pp},
-              po: {dr,po,pp},
-              eq: {dr},
-              pi: {dr},
-              pp: {dr,po,pp}},
-         po: {dr: {dr,po,pi},
-              po: {dr,po,eq,pi,pp},
-              eq: {po},
-              pi: {dr,po,pi},
-              pp: {po,pp}},
-         eq: {dr: {dr},
-              po: {po},
-              eq: {eq},
-              pi: {pi},
-              pp: {pp}},
-         pp: {dr: {dr},
-              po: {dr,po,pp},
-              eq: {pp},
-              pi: {dr,po,eq,pi,pp},
-              pp: {pp}},
-         pi: {dr: {dr,po,pi},
-              po: {po,pi},
-              eq: {pi},
-              pi: {pi},
-              pp: {po,eq,pp,pi}}}
+# each row [r1,r2,r] describes a case where B r1 D, C r2 D, and A r D,
+# where A is covered by B and C.
+table = [[dr,dr,{dr}],
+         [dr,eq,{pi}],
+         [dr,po,{po}],
+         [dr,pp,{po}],
+         [dr,pi,{pi}],
+         [po,po,{po,pi}],
+         [po,pp,{po,pi}],
+         [pp,pp,{eq,pp}]]
+                   
 
-
-def infer_rels(xy_rels, yz_rels):
-    Mxy = set()
-    for xy_rel in xy_rels:
-        for yz_rel in yz_rels:
-            Mxy = Mxy.union(table[xy_rel][yz_rel])
-    return Mxy
-
-
-def inv(rels):
-    inv_rels = set()
-    for rel in rels:
-        if rel == pp:
-            inv_rels.add(pi)
-        elif rel == pi:
-            inv_rels.add(pp)
-        else:
-            inv_rels.add(rel)
-    return inv_rels
-
-
-def find_rel(i, j, given_rels):
-    for (i0, j0, rel) in given_rels:
-        if i == i0 and j == j0:
-            return rel
-        elif i == j0 and j == i0:
-            return inv(rel)
-    return {dr,eq,po,pp,pi}
-                    
-
-# input:
-#     list of rels [(i,j,rels), ...]
-#     number of nodes n
-def path_consistent(given_rels, n):
-    # create an nxn matrix
-    matrix = [[{} for j in range(n)] for i in range(n)]
-    for i in range(n):
-        for j in range(n):
-            if i != j:
-                matrix[i][j] = find_rel(i, j, given_rels)
-    # queue of rels to infer
-    queue = []
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                if i != j and j != k and i != k:
-                    queue.append((i,j,k))
-    # calculate new rels until we reach a fixpoint
-    while queue != []:
-        i, k, j = queue.pop()
-        oldM = matrix[i][j]
-        Mij = infer_rels(matrix[i][k], matrix[k][j])
-        Mij = oldM.intersection(Mij)
-        if Mij == set():
-            return False
-        if oldM != Mij:
-            matrix[i][j] = Mij
-            matrix[j][i] = inv(Mij)
-            queue.append((i, k, j))
-
-    return matrix
+def bottom_up_inf(r1,r2):
+    r = set()
+    for row in table:
+        if row[0] in r1 and row[1] in r2:
+            r.update(row[2])
+        if row[0] in r2 and row[1] in r1:
+            r.update(row[2])
+    return r
 
 
 def as_str(rels):
@@ -113,7 +46,7 @@ def as_str(rels):
     for r in rels:
         s += r
     return s
-
+    
 
 def main():
     # R32 (minus empty)
@@ -126,15 +59,31 @@ def main():
             {dr,eq,pp}, {dr,eq,po}, {dr,eq,po,pp}, {dr,eq,pi},
             {dr,eq,pi,pp}, {dr,eq,pi,po}, {dr,eq,pi,po,pp}]
     n = len(rcc5)
+    rules = set()
+    # bottom up cases
     for i in range(n):
         for j in range(n):
-            given_rels = [(0,1,rcc5[i]),(1,2,rcc5[j])]
-            matrix = path_consistent(given_rels, 3)
-            r1 = as_str(matrix[0][1])
-            r2 = as_str(matrix[1][2])
-            r3 = as_str(matrix[0][2])
-            print 'compose(' + r1 + ',' + r2 + ',' + r3 + ').'
-                 
+            r1 = rcc5[i]
+            r2 = rcc5[j]
+            r = bottom_up_inf(r1, r2)
+            t1 = 'r('+as_str(r1)+',B,D)'
+            t2 = 'r('+as_str(r2)+',C,D)'
+            t3 = 'r('+as_str(r)+',A,D)'
+            if len(r) > 0:
+                rules.add(t3 + ' :- ' + 'tt(A,B,C), ' + t1 + ', ' + t2 + '.')
+    # top down cases
+    for i in range(n):
+        r = rcc5[i]
+        r1 = top_down_inf(r)
+        t1 = 'r('+as_str(r1)+',B,D)'
+        t2 = 'r('+as_str(r1)+',C,D)'
+        t3 = 'r('+as_str(r)+',A,D)'
+        if len(r1) > 0:
+            rules.add(t1 + ' :- ' + 'tt(A,B,C), ' + t3 + '.')
+            rules.add(t2 + ' :- ' + 'tt(A,B,C), ' + t3 + '.')            
+    for rule in rules:
+        print rule
+
 
 if __name__ == '__main__':
     main()
