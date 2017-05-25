4cb851f40b555a22c714b37bfeefa648cab26783
Shawn Bowers <smbowers@gmail.com>
2017-05-25 13:46:31 -0700

    getting closer, but not quite there

--- Datalog/tinytreetable.py
+++ Datalog/tinytreetable.py
@@ -34,16 +34,25 @@ def bottom_up_inf(r1,r2):
     for row in table:
         if row[0] in r1 and row[1] in r2:
             r.update(row[2])
-        if row[0] in r2 and row[1] in r1:
-            r.update(row[2])
     return r
 
 
+def top_down_inf(r, r1):
+    r2 = set()
+    for row in table:
+        if len(row[2].intersection(r)) > 0:
+            if row[0] in r1:
+                r2.add(row[1])
+            if row[1] in r1:
+                r2.add(row[0])
+    return r2
+
+
 def as_str(rels):
-    rels = list(rels)
-    rels.sort()
+    list_rels = list(rels)
+    list_rels.sort()
     s = ''
-    for r in rels:
+    for r in list_rels:
         s += r
     return s
     
@@ -59,8 +68,8 @@ def main():
             {dr,eq,pp}, {dr,eq,po}, {dr,eq,po,pp}, {dr,eq,pi},
             {dr,eq,pi,pp}, {dr,eq,pi,po}, {dr,eq,pi,po,pp}]
     n = len(rcc5)
-    rules = set()
     # bottom up cases
+    rules = set()
     for i in range(n):
         for j in range(n):
             r1 = rcc5[i]
@@ -71,16 +80,26 @@ def main():
             t3 = 'r('+as_str(r)+',A,D)'
             if len(r) > 0:
                 rules.add(t3 + ' :- ' + 'tt(A,B,C), ' + t1 + ', ' + t2 + '.')
+    print '% bottom-up coverage rules'
+    rules = list(rules)
+    rules.sort()
+    for rule in rules:
+        print rule
     # top down cases
+    rules = set()
     for i in range(n):
-        r = rcc5[i]
-        r1 = top_down_inf(r)
-        t1 = 'r('+as_str(r1)+',B,D)'
-        t2 = 'r('+as_str(r1)+',C,D)'
-        t3 = 'r('+as_str(r)+',A,D)'
-        if len(r1) > 0:
-            rules.add(t1 + ' :- ' + 'tt(A,B,C), ' + t3 + '.')
-            rules.add(t2 + ' :- ' + 'tt(A,B,C), ' + t3 + '.')            
+        for j in range(n):
+            r = rcc5[i]
+            r1 = rcc5[j]
+            r2 = top_down_inf(r, r1)
+            t1 = 'r('+as_str(r)+',A,D)'
+            t2 = 'r('+as_str(r1)+',B,D)'
+            t3 = 'r('+as_str(r2)+',C,D)'
+            if len(r2) > 0:
+                rules.add(t3 + ' :- ' + 'tt(A,B,C), ' + t1 + ', ' + t2 + '.')
+    print '% top-down coverage rules'
+    rules = list(rules)
+    rules.sort()
     for rule in rules:
         print rule
 
